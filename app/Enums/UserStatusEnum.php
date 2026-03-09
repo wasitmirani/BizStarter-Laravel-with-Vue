2e17c9f72f4ad88fe5be1b9e65222d92a950280a
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2026-03-10 01:14:33 +0500

    add: userstatus enum

---
+++ app/Enums/UserStatusEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum UserStatusEnums : string
+{
+
+    case Active = 'active';
+    case Pending = 'pending';
+    case Inactive = 'inactive';
+    case Suspended = 'suspended';
+    case Banned = 'banned';
+    case Deleted = 'deleted';
+
+}
