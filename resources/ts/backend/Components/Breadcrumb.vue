05d4ef1c8f5a5c9cb44777e783d8b21c25af7590
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2026-03-13 23:00:41 +0500

    fix: breadcrumb links issues

--- resources/ts/backend/Components/Breadcrumb.vue
+++ resources/ts/backend/Components/Breadcrumb.vue
@@ -8,16 +8,22 @@ const props = defineProps(['current', 'links'])
         <h4 class="page-main-title">{{ props.current }}</h4>
 
         <div class="hidden items-center gap-1.25 text-sm md:flex">
-
             <router-link to="/" class="text-sm">Home</router-link>
-
             <i class="iconify tabler--chevron-right text-sm rtl:rotate-180"></i>
 
-            <a href="#" class="text-sm" v-for="link in props.links" :key="link.id">{{ link.name }}</a>
-
+            <template v-for="(link, idx) in props.links" :key="link.id">
+                <router-link
+                    :to="{ name: link.route }"
+                    class="text-sm"
+                >
+                    {{ link.name }}
+                </router-link>
+                <i v-if="Number(idx) < props.links.length - 1"
+                    class="iconify tabler--chevron-right text-sm rtl:rotate-180"
+                ></i>
+            </template>
             <i class="iconify tabler--chevron-right text-sm rtl:rotate-180"></i>
-
-            <a href="#" class="text-default-400 text-sm" aria-current="page">{{ props.current }}</a>
+            <span class="text-default-400 text-sm" aria-current="page">{{ props.current }}</span>
         </div>
     </div>
 </template>
